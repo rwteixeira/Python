ae721fc2fc4c47bf055f92796776d5907da7bb2d
Ricardo Wagner Teixeira <ricardo@r4.com.br>
2025-08-06 17:03:49 -0300

    Aulas Python

---
+++ Curso de Python/Aula_25_arquivos/aula025.py
@@ -0,0 +1,23 @@
+# ====================== LIDANDO COM ARQUIVOS =======================
+# Crie um programa que peça e continue pedindo para que o usuário
+# informe palavras e vá armazenando tais palavras em um arquivo. 
+# Quando o usuário digitar '/exit', você deve interromper as leituras
+# (a palavra '/exit' não deve ser armazenada no arquivo).
+# Por fim, imprima na tela o conteúdo do arquivo.
+# 
+
+nome_arquivo = input("Nome do arquivo: ")
+
+with open(nome_arquivo, 'w') as arquivo:
+    while True:
+        palavra = input("Digite um palavra: \033[35m/exit\033[m para encerrar: ").strip()
+        if palavra == '/exit':
+            break
+        arquivo.write(f"{palavra}\n")
+
+print("Palavras armazendas com sucesso!\n")
+
+with open(nome_arquivo, 'r') as arquivo:
+        conteudo = arquivo.read()
+        print("Conteúdo do arquivo:")
+        print(conteudo)
