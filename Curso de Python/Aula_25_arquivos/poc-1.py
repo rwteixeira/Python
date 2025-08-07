1008f7a7eee14d147d19f477f1d7ef4e119c1ab6
Ricardo Wagner Teixeira <ricardo@r4.com.br>
2025-08-07 09:12:48 -0300

    Update poc-1.py

--- Curso de Python/Aula_25_arquivos/poc-1.py
+++ Curso de Python/Aula_25_arquivos/poc-1.py
@@ -1,9 +1,15 @@
 import os
-
-# Cria (ou sobrescreve) um arquivo e escreve conteúdo nele
-with open("exemplo.txt", "w", encoding="utf-8") as arquivo:
-    arquivo.write("Olá, Ricardo!\n")
-    arquivo.write("Este é um exemplo de manipulação de arquivos.\n")
-
 diretorio = os.getcwd()
-os.
+
+arquivo = str(input("Digite o nome do arquivo: "))
+
+fqn = diretorio
+# Cria (ou sobrescreve) um arquivo e escreve conteúdo nele
+# with open("exemplo.txt", "w", encoding="utf-8") as arquivo:
+#     arquivo.write("Olá, Ricardo!\n")
+#     arquivo.write("Este é um exemplo de manipulação de arquivos.\n")
+
+print(f"O nome e o caminho: {fqn}")
+print() 
+# print(f"O diretório de trabalho é: {diretorio}")
+
